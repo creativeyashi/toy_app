afdf07e21f4a339018b09f9d7aa42193a6618400
Anmol Gupta <yashikagupta082@gmail.com>
2022-02-22 19:32:08 +0530

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: "Hello,Yashika!"
+       # render html: "Hello,Yashika!"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+ # get 'home/index'
+ # resources :microposts
+  #resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  
+  get 'home/about'
+  root 'home#index'
+  #root 'application#hello'
   # Defines the root path route ("/")
   # root "articles#index"
 end
