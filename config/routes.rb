116efa9b0eae19ffc5bdc2c9efbd8c62c4f7035b
Anmol Gupta <yashikagupta082@gmail.com>
2022-02-24 03:42:37 +0530

    migrate

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   #get 'home/about'
-  root 'users#index'
+  root 'users#index'   #root 'home#index'
   #root 'application#hello'
   # Defines the root path route ("/")
   # root "articles#index"
