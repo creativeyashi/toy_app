076176e4e738c41930542c3415e59df26cef5174
Anmol Gupta <yashikagupta082@gmail.com>
2022-02-24 05:19:54 +0530

    procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
  # get 'home/index'
- # resources :microposts
-  #resources :users
+ resources :microposts
+resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   #get 'home/about'
